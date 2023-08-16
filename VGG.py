5601b1826c53db087792e456d62fcd21653e25e1
ianzih <ianzih0730@gmail.com>
2023-08-16 18:00:50 +0800

    revise VGG.py model

--- VGG.py
+++ VGG.py
@@ -2,11 +2,37 @@ import torch
 import torch.nn as nn
 from utils import conv_layer_bn, Flatten, ALComponent, ContrastiveLoss, PredSimLoss, VICRIG, Layer_Classifier, Set_Local_Loss
 
-class VGG(nn.Module):
+class VGG_block(nn.Module):
     def __init__(self, args):
-        super(VGG, self).__init__()
+        super(VGG_block, self).__init__()
+        self.args = args
+        self.shape = 32
+        self.in_channels = 3
+        
+    def _make_layer(self, channel_size: list):
+        layers = []
+        for dim in channel_size:
+            if dim == 'M':
+                layers.append(nn.MaxPool2d(2, stride=2))
+            else:
+                layers.append(conv_layer_bn(self.in_channels, dim, nn.ReLU()))
+                self.in_channels = dim
+        return nn.Sequential(*layers) 
+    
+    def _shape_div_2(self):
+        self.shape //= 2
+        return self.shape
+    
+    def forward(self, x, y):
+        if self.training:
+            return self.train_step(x, y)
+        else:
+            return self.inference(x, y)
+    
+class VGG(VGG_block):
+    def __init__(self, args):
+        super(VGG, self).__init__(args)
         self.num_class = args.n_classes
-        self.in_channel = 3
         self.ce = nn.CrossEntropyLoss()
 
         self.layer1 = self._make_layer([128, 128, 128, 256, 'M'])
@@ -35,19 +61,7 @@ class VGG(nn.Module):
             return loss
         else:
             return out.detach()
-        
-    def _make_linear_layer(self, in_features, out_features):
-            return nn.Sequential(nn.Linear(in_features, out_features, bias=True), nn.BatchNorm1d(out_features), nn.ReLU())
-        
-    def _make_layer(self, channel_size: list):
-        layers = []
-        for dim in channel_size:
-            if dim == 'M':
-                layers.append(nn.MaxPool2d(2, stride=2))
-            else:
-                layers.append(conv_layer_bn(self.in_channel, dim, nn.ReLU()))
-                self.in_channel = dim
-        return nn.Sequential(*layers)
+
 
 class VGG_AL_Component(ALComponent):
     def __init__(self, conv:nn.Module, flatten_size: int, hidden_size: int, out_features: int):
@@ -65,9 +79,10 @@ class VGG_AL_Component(ALComponent):
         ca = nn.MSELoss()
         super(VGG_AL_Component, self).__init__(f, g, b, inv, cf, cb, ca)
 
-class VGG_AL(nn.Module):
+
+class VGG_AL(VGG_block):
     def __init__(self, args):
-        super(VGG_AL, self).__init__()
+        super(VGG_AL, self).__init__(args)
 
         layer_cfg = {0:[128, 256, "M"], 1:[256, 512, "M"], 2:[512, "M"], 3:[512, "M"]}
         neuron_size = 500
@@ -131,25 +146,12 @@ class VGG_AL(nn.Module):
             _t0 = self.layer2(None, _t0)
             _t0 = self.layer1(None, _t0)
             return _t0
-    
-    def _make_layer(self, channel_size):
-        layers = []
-        for dim in channel_size:
-            if dim == "M":
-                layers.append(nn.MaxPool2d(2, stride=2))
-                self.size/=2
-            else:
-                layers.append(conv_layer_bn(self.features, dim, nn.ReLU()))
-                self.features = dim
-        return nn.Sequential(*layers)
 
-class VGG_SCPL(nn.Module):
+
+class VGG_SCPL(VGG_block):
     def __init__(self, args):
-        super(VGG_SCPL, self).__init__()
+        super(VGG_SCPL, self).__init__(args)
         self.num_classes = args.n_classes
-
-        self.shape = 32
-        self.in_channels = 3
         # layer_cfg = {0:[128, 256, "M"], 1:[256, 512, "M"], 2:[512, "M"], 3:[512, "M"]}
         layer_cfg = {0:[128, 128, 128, 256, "M"], 1:[256, 512, "M"], 2:[512, 512, "M"], 3:[512, "M"]}
 
@@ -171,57 +173,50 @@ class VGG_SCPL(nn.Module):
 
         self.fc = nn.Sequential(Flatten(), nn.Linear(2048, 2048), nn.BatchNorm1d(2048), nn.ReLU(), nn.Linear(2048, self.num_classes))
         self.ce = nn.CrossEntropyLoss()
-
-        
-    def forward(self, x, y):
+    
+    def train_step(self, x, y):
         loss = 0
+        
         output = self.layer1(x)
-        if self.training:
-            loss += self.loss1(output, y)
-            output = output.detach()
+        loss += self.loss1(output, y)
+        output = output.detach()
+        
         output = self.layer2(output)
-        if self.training:
-            loss += self.loss2(output, y)
-            output = output.detach()
+        loss += self.loss2(output, y)
+        output = output.detach()
+        
         output = self.layer3(output)
-        if self.training:
-            loss += self.loss3(output, y)
-            output = output.detach()
+        loss += self.loss3(output, y)
+        output = output.detach()
+        
         output = self.layer4(output)
-        if self.training:
-            loss += self.loss4(output, y)
-            output = output.detach()
+        loss += self.loss4(output, y)
+        output = output.detach()
         
         output = self.fc(output)
-        if self.training:
-            loss += self.ce(output, y)
-            return loss 
-        else:
-            return output
-    
-    def _make_layer(self, channel_size):
-        layers = []
-        for dim in channel_size:
-            if dim == "M":
-                layers.append(nn.MaxPool2d(2, stride=2))
-            else:
-                layers.append(conv_layer_bn(self.in_channels, dim, nn.ReLU()))
-                self.in_channels = dim
-        return nn.Sequential(*layers)
-    
-    def _shape_div_2(self):
-        self.shape //= 2
-        return self.shape
+        loss += self.ce(output, y)
+        return loss 
 
+    def inference(self, x, y):
+        output = self.layer1(x)
 
-class VGG_SCPL_Dynamic(nn.Module):
+        output = self.layer2(output)
+
+        output = self.layer3(output)
+
+        output = self.layer4(output)
+
+        output = self.fc(output)
+
+        return output
+        
+
+class VGG_SCPL_Dynamic(VGG_block):
     def __init__(self, args):
-        super(VGG_SCPL_Dynamic, self).__init__()
+        super(VGG_SCPL_Dynamic, self).__init__(args)
         self.num_classes = args.n_classes
         self.merge = args.merge
         self.blockwisetotal = args.blockwise_total
-        self.shape = 32
-        self.in_channels = 3
         # layer_cfg = {0:[128, 256, "M"], 1:[256, 512, "M"], 2:[512, "M"], 3:[512, "M"]}
         # layer_cfg = {0: [64, 64, "M" ], 1:[128, 128, "M"], 2:[256, 256, 256, "M"], 3:[512, 512, 512, "M"], 4:[512, 512, 512, "M"]}
         layer_cfg = {0:[128, 128, 128, 256, "M"], 1:[256, 512, "M"], 2:[512, 512, "M"], 3:[512, "M"]}
@@ -244,87 +239,87 @@ class VGG_SCPL_Dynamic(nn.Module):
         
         self.ce = nn.CrossEntropyLoss()
 
-        
-    def forward(self, x, y):
+    def train_step(self, x, y):
         total_loss = 0
         total_classifier_loss = 0
+    
+        # Layer1
+        loss, classifier_loss, output = self._training_each_layer(x, y , self.layer1, self.loss1, self.classifier1)
+        total_loss += loss
+        total_classifier_loss += classifier_loss
+        
+        # Layer2    
+        loss, classifier_loss, output = self._training_each_layer(output, y , self.layer2, self.loss2, self.classifier2)
+        total_loss += loss
+        total_classifier_loss += classifier_loss
+            
+        # Layer3
+        loss, classifier_loss, output = self._training_each_layer(output, y , self.layer3, self.loss3, self.classifier3)
+        total_loss += loss
+        total_classifier_loss += classifier_loss
+            
+        # Layer4
+        loss, classifier_loss, output = self._training_each_layer(output, y , self.layer4, self.loss4, self.classifier4)
+        total_loss += loss
+        total_classifier_loss += classifier_loss
+             
+        return (total_loss + total_classifier_loss) 
+        
+    def inference(self, x, y):
         classifier_output = {i: [] for i in range(1, self.blockwisetotal + 1)}
     
         # Layer1
-        loss, classifier_loss, classifier_out, output = self._each_layer(x, y , self.layer1, self.loss1, self.classifier1)
+        classifier_out, output = self._inference_each_layer(x, y , self.layer1, self.loss1, self.classifier1)
         classifier_output[1].append(classifier_out)
-        if self.training: 
-            total_loss += loss
-            total_classifier_loss += classifier_loss
         
         # Layer2    
-        loss, classifier_loss, classifier_out, output = self._each_layer(output, y , self.layer2, self.loss2, self.classifier2)
+        classifier_out, output = self._inference_each_layer(output, y , self.layer2, self.loss2, self.classifier2)
         classifier_output[2].append(classifier_out)
-        if self.training: 
-            total_loss += loss
-            total_classifier_loss += classifier_loss
             
         # Layer3
-        loss, classifier_loss, classifier_out, output = self._each_layer(output, y , self.layer3, self.loss3, self.classifier3)
+        classifier_out, output = self._inference_each_layer(output, y , self.layer3, self.loss3, self.classifier3)
         classifier_output[3].append(classifier_out)
-        if self.training: 
-            total_loss += loss
-            total_classifier_loss += classifier_loss
             
         # Layer4
-        loss, classifier_loss, classifier_out, output = self._each_layer(output, y , self.layer4, self.loss4, self.classifier4)
+        classifier_out, output = self._inference_each_layer(output, y , self.layer4, self.loss4, self.classifier4)
         classifier_output[4].append(classifier_out)
-        if self.training: 
-            total_loss += loss
-            total_classifier_loss += classifier_loss
              
-        if self.training:
-            return (total_loss + total_classifier_loss) 
-        else:
-            return output ,  classifier_output
+        return output ,  classifier_output
     
-    def _make_layer(self, channel_size):
-        layers = []
-        for dim in channel_size:
-            if dim == "M":
-                layers.append(nn.MaxPool2d(2, stride=2))
-            else:
-                layers.append(conv_layer_bn(self.in_channels, dim, nn.ReLU()))
-                self.in_channels = dim
-        return nn.Sequential(*layers)
-    
-    def _shape_div_2(self):
-        self.shape //= 2
-        return self.shape
-    
-    def _each_layer(self, x, y , layer, localloss, classifier):
+    def _training_each_layer(self, x, y , layer, localloss, classifier):
         classifier_loss = 0
         output = layer(x)
         loss , projector_out= localloss(output, y)
-        if self.training:
-            output = output.detach()
-            # projector_out = projector_out.detach()
+
+        output = output.detach()
+        # projector_out = projector_out.detach()
+            
+        if self.merge == 'merge':
+            classifier_out = classifier(projector_out)
+        elif self.merge == 'unmerge':
+            classifier_out = classifier(output) 
+        classifier_loss = self.ce(classifier_out , y) * 0.001
+            
+        return loss , classifier_loss , output
+    
+    def _inference_each_layer(self, x, y , layer, localloss, classifier):
+        output = layer(x)
+        loss , projector_out= localloss(output, y)
             
         if self.merge == 'merge':
             classifier_out = classifier(projector_out)
         elif self.merge == 'unmerge':
             classifier_out = classifier(output)
-            
-        if self.training:
-            classifier_loss = self.ce(classifier_out , y) * 0.001
-            
-            
-        return loss , classifier_loss, classifier_out, output
+                  
+        return classifier_out, output
     
     
-class VGG_PredSim(nn.Module):
+class VGG_PredSim(VGG_block):
     def __init__(self, args):
-        super(VGG_PredSim, self).__init__()
+        super(VGG_PredSim, self).__init__(args)
         self.num_classes = args.n_classes
-
         self.num_layers = 4
         self.size = 32
-        self.features = 3
         layer_cfg = {0:[128, 256, "M"], 1:[256, 512, "M"], 2:[512, "M"], 3:[512, "M"]}
         conv_layers = []
         loss_funcs = []
@@ -338,7 +333,6 @@ class VGG_PredSim(nn.Module):
         self.conv = nn.ModuleList(conv_layers)
         self.cl = nn.ModuleList(loss_funcs)
         self.ce = nn.CrossEntropyLoss()
-
         
     def forward(self, x, label):
         if self.training:
@@ -361,16 +355,3 @@ class VGG_PredSim(nn.Module):
             y = self.fc(x)
 
             return y
-    
-    def _make_conv_layer(self, channel_size, activation):
-        layers = []
-        for dim in channel_size:
-            if dim == "M":
-                layers.append(nn.MaxPool2d(2, stride=2))
-                self.size/=2
-            else:
-                layers.append(conv_layer_bn(self.features, dim, activation))
-                self.features = dim
-        return nn.Sequential(*layers)
-    def _make_linear_layer(self, in_features, out_features):
-        return nn.Sequential(nn.Linear(in_features, out_features, bias=True), nn.BatchNorm1d(out_features), nn.ReLU())
